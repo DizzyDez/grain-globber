a9732f8a10b68383a1fac35fb34cba3f7314f512
DizzyDez <desc0137@algonquinlive.com>
2016-03-17 19:28:46 -0400

    first commit laying the ground work

---
+++ js/main.js
@@ -0,0 +1,12 @@
+grains.forEach(function (item){
+  var $ul = $('ul');
+  var $li = $('<li>');
+  var $img = $('<img>');
+  var $p = $('<p>');
+
+})
+
+  $ul.append($li);
+  $img.attr('src', 'images/' + item.img);
+  $li.append($img);
+  $h2.html(item.name);
